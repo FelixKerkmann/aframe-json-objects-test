adaef9759882cc065402cd2ae6e624fcae397589
FelixKerkmann <felix.kerkmann@students.fhnw.ch>
2022-05-04 14:25:20 +0200

    models in mongoDB

--- app.js
+++ app.js
@@ -4,104 +4,74 @@ const bodyParser = require('body-parser');
 const path = require('path');
 let ejs = require('ejs');
 const json2html = require('node-json2html');
-const multer = require("multer")
+const multer = require("multer");
+const DbConnectionString = 'mongodb+srv://aframe:Owi93zQUubpASo5V@cluster0.u4bj4.mongodb.net/aframe-prototype?retryWrites=true&w=majority'
+const MongoClient = require('mongodb').MongoClient
 const app = express();
 
 // View Engine Setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use('/uploads', express.static(__dirname + '/uploads'));
+MongoClient.connect(DbConnectionString, { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to Database');
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(express.static('public'));
+        app.use('/uploads', express.static(__dirname + '/uploads'));
+        const db = client.db('aframe-gltf-models');
+        const modelsCollection = db.collection('models');
 
-app.get('/listEntities', function (req, res) {
-    fs.readFile( "data.json", 'utf8', function (err, data) {
-        console.log( data );
-        res.end( data );
-    });
-})
+        app.get('/form', function (req, res){
+            res.render('form', );
+        })
 
-app.get('/form', function (req, res){
-    res.render('form', );
-})
-/*
-app.get('/', function (req, res) {
-    fs.readFile( "data.json", 'utf8', function (err, data) {
-        let template = {'<>':'${entity}','position':'${positionx} ${positiony} ${positionz}','color':'${color}'}
-        console.log(data);
-        console.log(template);
-        let html = json2html.render(data, template);
-        console.log(html);
-        res.render('view', {
-            entities: ejs.render(html)
+        app.get('/', function (req, res) {
+
+            modelsCollection.find().toArray()
+                .then(results => {
+                    console.log(results)
+                    let template = {'<>':'${entity}', 'gltf-model': 'url(/uploads/${fname})', 'position':'${positionx} ${positiony} ${positionz}', 'rotation':'${rotationx} ${rotationy} ${positionz}','scale':'${scale} ${scale} ${scale}'};
+                    console.log(template);
+                    let html = json2html.render(results, template);
+                    console.log(html);
+                    res.render('view', {
+                        entities: ejs.render(html)
+                    });
+                })
+                .catch(error => console.error(error))
         });
-        //<a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-    });
-})
-*/
-app.get('/', function (req, res) {
-    fs.readFile( "objects.json", 'utf8', function (err, data) {
-        let template = {'<>':'${entity}', 'gltf-model': 'url(/uploads/${id})', 'position':'${positionx} ${positiony} ${positionz}', 'rotation':'${rotationx} ${rotationy} ${positionz}','scale':'${scale} ${scale} ${scale}'};
-        console.log(template);
-        let html = json2html.render(data, template);
-        console.log(html);
-        res.render('view', {
-            entities: ejs.render(html)
+
+        const storage = multer.diskStorage({
+            destination: function (req, file, cb) {
+                cb(null, 'uploads')
+            },
+            filename: function (req, file, cb) {
+                cb(null, file.originalname)
+            }
+        })
+
+        const upload = multer({ storage: storage })
+
+        app.post('/object', upload.single("gltffile"), function (req,res) {
+            //console.log(req.file, req.body)
+            modelsCollection.insertOne({
+                entity: "a-entity",
+                fname: req.file.originalname,
+                positionx: parseInt(req.body.positionx),
+                positiony: parseInt(req.body.positiony),
+                positionz: parseInt(req.body.positionz),
+                rotationx: parseInt(req.body.rotationx),
+                rotationy: parseInt(req.body.rotationy),
+                rotationz: parseInt(req.body.rotationz),
+                scale: parseInt(req.body.scale)
+            })
+            res.redirect('/');
+        })
+
+        const server = app.listen(8888, function () {
+            console.log("Json Entities app listening at http://localhost:8888");
         });
-        //<a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-    });
-})
+    })
+    .catch(console.error)
 
-app.post('/form', function (req,res) {
-    response = {
-        entity:req.body.entity,
-        positionx:parseInt(req.body.positionx),
-        positiony:parseInt(req.body.positiony),
-        positionz:parseInt(req.body.positionz),
-        color:req.body.color
-    }
-    let cur = fs.readFileSync('data.json');
-    let objects = JSON.parse(cur);
-    ;objects.push(response)
-    let newData = JSON.stringify(objects);
-    fs.writeFileSync('data.json', newData);
-    res.redirect('/');
-})
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads')
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
-    }
-})
-
-const upload = multer({ storage: storage })
-
-app.post('/object', upload.single("gltffile"), function (req,res) {
-    //console.log(req.file, req.body)
-    response = {
-        entity:"a-entity",
-        id:req.file.originalname,
-        positionx:parseInt(req.body.positionx),
-        positiony:parseInt(req.body.positiony),
-        positionz:parseInt(req.body.positionz),
-        rotationx:parseInt(req.body.rotationx),
-        rotationy:parseInt(req.body.rotationy),
-        rotationz:parseInt(req.body.rotationz),
-        scale:parseInt(req.body.scale)
-    }
-    let cur = fs.readFileSync('objects.json');
-    var objects = JSON.parse(cur);
-    objects.push(response);
-    var newData = JSON.stringify(objects);
-    fs.writeFileSync('objects.json', newData);
-    res.redirect('/');
-})
-
-const server = app.listen(8888, function () {
-    const host = server.address().address;
-    const port = server.address().port;
-    console.log("Json Entities app listening at http://%s:%s", host, port);
-});
