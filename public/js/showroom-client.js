366a86c3f7e5a0f601a10e82998c80642af28080
FelixKerkmann <felix.kerkmann@students.fhnw.ch>
2022-08-04 14:54:49 +0200

    socket disconnection client

--- public/js/showroom-client.js
+++ public/js/showroom-client.js
@@ -3,6 +3,15 @@ const SHOWROOM = 'showroomid'
 const OBJECT_SELECTOR = '#ObjectSelector'
 let socket = io();
 
+
+socket.on("connect", () => {
+    console.log("socket connection established")
+})
+
+socket.on("disconnect", () => {
+    location.reload()
+})
+
 socket.on('updateValuesSuccess', (name, keys, oldValues, newValues) => {
     console.log('Update ' + updateValuesToString(keys, oldValues, newValues) + ' deployed successfully in database.');
 })
