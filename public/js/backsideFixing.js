2e80b648e07c2edeb5c6ffdfb9d489dcb49e5ce4
FelixKerkmann <felix.kerkmann@students.fhnw.ch>
2022-08-05 15:55:26 +0200

    bug fix

--- public/js/backsideFixing.js
+++ public/js/backsideFixing.js
@@ -4,40 +4,41 @@ function setAllBackfaces(){
 
     elementsInScene.forEach(setBacksideOfModel);
 
-    function setBacksideOfModel(model){
-        if(isTheRoom(model) || isASelectable(model)){
-            model.children.forEach(setBackside);
-        }
+}
+
+function setBacksideOfModel(model){
+    if(isTheRoom(model) || isASelectable(model)){
+        model.children.forEach(setBackside);
+    }
+}
+
+function isTheRoom(element){
+    try{
+        return element.el.id === 'Room';
+
+    }catch(_){
+        return false; // Has no el (not an a-frame entity)
+    }
+}
+
+function isASelectable(element){
+    try{
+        return element.el.getAttribute('selectable') !== null;
+
+    }catch(_){
+        return false; // Has no selectable component
+    }
+}
+
+function setBackside(element)
+{
+    if(element.type === 'Group'){
+        element.children.forEach(setBackside);
+        return;
     }
 
-    function isTheRoom(element){
-        try{
-            return element.el.id === 'Room';
-
-        }catch(_){
-            return false; // Has no el (not an a-frame entity)
-        }
-    }
-
-    function isASelectable(element){
-        try{
-            return element.el.getAttribute('selectable') !== null;
-
-        }catch(_){
-            return false; // Has no selectable component
-        }
-    }
-
-    function setBackside(element)
-    {
-        if(element.type === 'Group'){
-            element.children.forEach(setBackside);
-            return;
-        }
-
-        if(element.type === 'Mesh'){
-            element.material.shadowSide = THREE.BackSide;
-            console.log('Changed backside of ' + element.name + ' of ' + element.parent.name);
-        }
+    if(element.type === 'Mesh'){
+        element.material.shadowSide = THREE.BackSide;
+        console.log('Changed backside of ' + element.name + ' of ' + element.parent.name);
     }
 }
